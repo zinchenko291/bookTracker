b7a6ac3f3e4ed0652a002785a03d7c7d1f656148
zinchenko291 <zinchenko291@gmail.com>
2020-08-20 01:34:17 +0500

    Added sass and bootstrap, navbar laid out

--- web/app/index.php
+++ web/app/index.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="libs/bootstrap-4.5.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.min.css">
     <title>Book Tracker</title>
 </head>
 <body>
-    
+
+<nav id="nav" class="navbar">
+    <div class="container">
+        <a href="index.php" class="navbar-brand" id="nav-brand">
+            <img src="img/book.svg" alt="" width="30" height="30" class="d-inline-block align-top" loading="lazy">
+            Book Tracker
+        </a>
+        <form class="form-inline">
+            <a class="btn btn-outline-white">Войти</a>
+        </form>
+    </div>
+</nav>
+
 </body>
 </html>
